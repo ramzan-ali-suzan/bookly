c495571d4e701e5b65d6e09ec12cf3cccd8a78c7
Muhammad Ramzan Ali Suzan <muhammad.ramzan@brainstation23.com>
2021-02-10 12:19:57 +0600

    Books Component Done

--- src/components/books.jsx
+++ src/components/books.jsx
@@ -1,9 +1,53 @@
 import React, { Component } from "react";
+import { getBooks } from "./../services/fakeBookService";
 
 class Books extends Component {
-  state = {};
+  state = {
+    books: getBooks(),
+  };
+
+  handleDelete = (book) => {
+    const books = this.state.books.filter((m) => m._id !== book._id);
+    this.setState({ books });
+  };
+
   render() {
-    return <h2>Books Component</h2>;
+    const { length: count } = this.state.books;
+    if (count === 0) return <p>There is no Books in the database!</p>;
+    return (
+      <React.Fragment>
+        <p>Showing {count} books in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Author</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.books.map((book) => (
+              <tr key={book._id}>
+                <td>{book.title}</td>
+                <td>{book.genre.name}</td>
+                <td>{book.numberInStock}</td>
+                <td>{book.author}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(book)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
   }
 }
 
